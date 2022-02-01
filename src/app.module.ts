b9bb1ba5c7c75d6e938a1f357b7adb3df6a3d45d
Fedka Katorjnii <fedkakatorjniy@gmail.com>
2022-02-01 22:42:41 +0300

    feat(): настройка подключение БД

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { getConnectionOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          entities: ['../entities/*.entity.{ts,js}'],
+          migrations: ['../migrations/*.{ts,js}'],
+        }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
